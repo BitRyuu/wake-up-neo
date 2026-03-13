490f1be7ff5940e20344a1b1344b8431d713b3f4
sagar b <sagarbhajantri9945@gmail.com>
2026-03-13 21:02:33 +0530

    Initial Commit

---
+++ Wake up Neo/Wake_up_NeoView.h
@@ -0,0 +1,12 @@
+//
+//  Wake_up_NeoView.h
+//  Wake up Neo
+//
+//  Created by sagar b on 13/03/26.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Wake_up_NeoView : ScreenSaverView
+
+@end
---
+++ Wake up Neo/Wake_up_NeoView.m
@@ -0,0 +1,51 @@
+//
+//  Wake_up_NeoView.m
+//  Wake up Neo
+//
+//  Created by sagar b on 13/03/26.
+//
+
+#import "Wake_up_NeoView.h"
+
+@implementation Wake_up_NeoView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
